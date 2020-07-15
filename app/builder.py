7c22bc61b739444a553f5810844e8ba95bc83ac1
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-07-15 15:12:22 -0400

    add volumes function

--- app/builder.py
+++ app/builder.py
@@ -16,8 +16,24 @@ print ("> runing on region: {0}".format(region))
 def profile_session(reg = None):
     if reg is None:
         reg = region
-    session = boto3.session.Session(profile_name=reg)
-    return session.resource('ec2')
+    s = boto3.session.Session(profile_name=reg)
+    return s.resource('ec2')
+
+def blockdev(vol):
+    ebs = {}
+    if 'device' in vol:
+        ebs= {
+                'DeviceName': vol['device'],
+                'Ebs': {
+                    'VolumeSize': vol['size_gb'],
+                    'DeleteOnTermination': True,
+                }
+            }
+            
+        
+    else:
+        print("{0} volumes not found.".format(sys.argv[2]))
+    return ebs
 
 
 def build():
@@ -28,13 +44,17 @@ def build():
         except IOError:
             print( "{0} not found.".format(sys.argv[2]))
             sys.exit(1)
-        for reg in yt: # loop through regions
-            ec2 = profile_session(reg)
-            for azlst in yt[reg]: # loop through AZ list
-                for az in azlst: # loop through AZ
-                    for instance in azlst[az]:    
-                        print(instance['name'])
-
+        for profile in yt: # profiles loop
+            ec2 = profile_session(profile)
+            map = []
+            for server in yt[profile]: # loop through servers list
+                print(server['server_name'])
+                i = 0
+                for vol in server['volumes']: # loop through AZ
+                    map.append(blockdev(vol))
+                    #for instance in server[vol]:    
+                     #   print(instance['name'])
+                print("creating instance {0}".format(server['server_name']))
 def delete():
     if len(sys.argv) < 3:
         print("usage: {0} delete <aws instance id>")
