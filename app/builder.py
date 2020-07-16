e9315b6887dbc4547bfb0d2441846eaadd33ce15
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-07-16 01:10:53 -0400

    added sme helper function to create keys and instances

--- app/builder.py
+++ app/builder.py
@@ -4,73 +4,102 @@ import boto3
 import os
 import sys
 import yaml
+import configparser
 
-region = os.environ.get('AWSREGION')
+config = configparser.ConfigParser()
+config.read('../config.ini')
+region = config['DEFAULT']['AWSREGION']
 
-if region is None:
-    print ("Please set the AWSREGION environment variable.")
-    sys.exit(1)
+print("> running on region: {0}".format(region))
 
-print ("> runing on region: {0}".format(region))
 
-def profile_session(reg = None):
+def ec2_session(reg=None):
     if reg is None:
         reg = region
     s = boto3.session.Session(profile_name=reg)
     return s.resource('ec2')
 
+
 def blockdev(vol):
+    print('creating ebs vols')
     ebs = {}
     if 'device' in vol:
-        ebs= {
-                'DeviceName': vol['device'],
-                'Ebs': {
-                    'VolumeSize': vol['size_gb'],
-                    'DeleteOnTermination': True,
-                }
+        ebs = {
+            'DeviceName': vol['device'],
+            'Ebs': {
+                'VolumeSize': vol['size_gb'],
+                'DeleteOnTermination': True,
             }
-            
-        
+        }
+
     else:
         print("{0} volumes not found.".format(sys.argv[2]))
     return ebs
 
 
+def create_key(ec2, key_name):
+    print('create key')
+    keypair = ec2.create_key_pair(KeyName=key_name)
+    return keypair
+
+
+def user_data():
+    print('user data')
+    return ''
+# build the instance
+
+
 def build():
     if validate_template() == True:
+
         try:
             with open(sys.argv[2], 'r') as f:
                 yt = yaml.safe_load(f.read())
         except IOError:
-            print( "{0} not found.".format(sys.argv[2]))
+            print("{0} not found.".format(sys.argv[2]))
             sys.exit(1)
-        for profile in yt: # profiles loop
-            ec2 = profile_session(profile)
+        #
+        for profile in yt:  # profiles loop
+            ec2 = ec2_session(profile)
             blockdevmap = []
-            for server in yt[profile]: # loop through servers list
-                print(server['server_name'])
-                i = 0
-                for vol in server['volumes']: # loop through AZ
+            for server in yt[profile]:  # loop through servers list
+                print(server['server_name']
+                      )
+
+                for vol in server['volumes']:  # loop through AZ
                     blockdevmap.append(blockdev(vol))
-                    #for instance in server[vol]:    
-                     #   print(instance['name'])
-                print("creating instance {0}".format(server['server_name']))
-                rc = ec2.create_instances(
-                        MinCount = 1,
-                        MaxCount = 1,
-                        KeyName = server['users']['login'],
-                        InstanceType = server['server_type'],
-                        BlockDeviceMappings = blockdevmap
-                    )
-                server_id = rc[0].id
+                for user in server['users']:
+                        pk = create_key(ec2,user['login'])
+                        print(pk)
+                server_id = create_instance(server, ec2, blockdevmap)
                 print("Created server id {0}".format(server_id))
+
+
+# creating the EC2 instance
+def create_instance(server, ec2, blockdevmap):
+
+    rc = ec2.create_instances(
+        MinCount=1,
+        MaxCount=1,
+        KeyName = server['users']['login'],
+        InstanceType=server['server_type'],
+        ImageId='ami-042760c4b14cf6044',
+        BlockDeviceMappings=blockdevmap
+    )
+    server_id = rc[0].id
+    ec2.create_tags(
+        Resources=[server_id],
+        Tags=name_tag(server['server_name'])
+    )
+    return server_id
+
 
 def delete():
     if len(sys.argv) < 3:
         print("usage: {0} delete <aws instance id>")
         sys.exit(1)
 
-    ec2 = profile_session()
+    ec2 = ec2_session()
 
     try:
         ec2.instances.filter(InstanceIds=[sys.argv[2]]).terminate()
@@ -78,8 +107,10 @@ def delete():
         print('error deleting {0}'.format(sys.argv[2]))
         sys.exit(1)
 
+
 def validate_template():
     return True
+
 
 def get_id_by_name(ec2obj, tag):
     for o in ec2obj.filter(Filters=[{'Name': 'tag:Name', 'Values': [tag]}]):
@@ -87,9 +118,9 @@ def get_id_by_name(ec2obj, tag):
 
     return None
 
+
 def name_tag(val):
     return [{'Key': 'Name', 'Value': val}]
-
 
 
 if __name__ == '__main__':
